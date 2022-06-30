4c649d518686b59e17bff81c1e45f022dac871ee
NotHyper <survivaltemer@gmail.com>
2022-06-29 21:25:47 -0300

    Update FPSCamera.cs
    Add simple mouse lock

--- Assets/Scripts/Player/FPSCamera.cs
+++ Assets/Scripts/Player/FPSCamera.cs
@@ -4,29 +4,38 @@ using UnityEngine;
 
 public class FPSCamera : MonoBehaviour
 {
-	[SerializeField] private float sensitivity = 2f;
-	[SerializeField] private float MinPitch = -90f;
-	[SerializeField] private float MaxPitch = 90f;
+#pragma warning disable CS0618 // Convenience
+	[SerializeField] private float m_Sensitivity = 2f;
+	[SerializeField] private float m_MinPitch = -90f;
+	[SerializeField] private float m_MaxPitch = 90f;
+	[SerializeField] private bool m_LockMouse = true;
 	
 	private Vector3 eulerRot;
 	
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+		Screen.lockCursor = m_LockMouse;
+	}
 
     // Update is called once per frame
     void Update()
     {
-        float yaw = Input.GetAxisRaw("Mouse X") * sensitivity;
-		float pitch = -Input.GetAxisRaw("Mouse Y") * sensitivity;
+        float yaw = Input.GetAxisRaw("Mouse X") * m_Sensitivity;
+		float pitch = -Input.GetAxisRaw("Mouse Y") * m_Sensitivity;
 		
 		eulerRot.x += pitch;
-		eulerRot.x = Mathf.Clamp(eulerRot.x, MinPitch, MaxPitch);
+		eulerRot.x = Mathf.Clamp(eulerRot.x, m_MinPitch, m_MaxPitch);
 		eulerRot.y += yaw;
 		
 		transform.localRotation = Quaternion.Euler(new Vector3(eulerRot.x, 0f));
 		transform.parent.localRotation = Quaternion.Euler(new Vector3(0f,eulerRot.y));
+
+		if (Input.GetButtonDown("Cancel"))
+		{
+			m_LockMouse = !m_LockMouse;
+			Screen.lockCursor = m_LockMouse;
+		}
     }
 }
+#pragma warning restore CS0618
